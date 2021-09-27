a1dd9a9380e85b67ea76cdfbb81be60e8d95844c
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-09-27 21:19:12 +0300

    add basic provider to ContextAttributes

--- src/main/java/org/example/framework/attribute/ContextAttributes.java
+++ src/main/java/org/example/framework/attribute/ContextAttributes.java
@@ -1,6 +1,7 @@
 package org.example.framework.attribute;
 
 public class ContextAttributes {
+  public static final String BASIC_PROVIDER_ATTR = "org.example.framework.BASIC_PROVIDER";
   public static final String AUTH_PROVIDER_ATTR = "org.example.framework.AUTH_PROVIDER";
   public static final String ANON_PROVIDER_ATTR = "org.example.framework.ANON_PROVIDER";
   public static final String JDBC_TEMPLATE_ATTR = "org.example.framework.JDBC_TEMPLATE";
