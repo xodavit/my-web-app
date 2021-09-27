15fb0cc760370700cab0e0e2087643a14e63eb39
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-09-27 09:11:12 +0300

    add roles

--- src/main/java/org/example/framework/security/Roles.java
+++ src/main/java/org/example/framework/security/Roles.java
@@ -4,4 +4,8 @@ public class Roles {
   private Roles() {}
 
   public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
+  public static final String ROLE_ADMIN = "ROLE_ADMIN";
+  public static final String ROLE_MODERATOR = "ROLE_MODERATOR";
+  public static final String ROLE_USER = "ROLE_USER";
+  public static final String ROLE_STUDENT = "ROLE_STUDENT";
 }
